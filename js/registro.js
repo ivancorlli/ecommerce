6024c521e70a4be6b8bd4aa83b65f70164023298
cristianzanesky11 <crisz_zanesky@hotmail.com>
2021-08-02 21:34:47 -0300

    envio mail

--- js/registro.js
+++ js/registro.js
@@ -5,30 +5,37 @@ function registrarUsuario() {
     let subname= document.getElementById("subname").value;  
     let email= document.getElementById("email").value;
     let password= document.getElementById("password").value;
-    console.log(name,subname,email,password) ;
     let user = {
         name,
         subname,
         email,
         password,
     }
+
+    
     oldUser.push(user);
     localStorage.setItem('users',JSON.stringify(oldUser));
-
+    
+    let params= {
+        from: 'crisz_zanesky@hotmail.com',
+        to: 'criszanesky18@gmail.com',
+        message: `Nuevo Usuario ${name} ${subname} ${email} ${password}`,
+    };
+     
+    emailjs.send('service_9n5wu8g', 'template_zy2hlk8', params)
+        .then(function(response) {
+           console.log('SUCCESS!', response.status, response.text);
+        }, function(error) {
+           console.log('FAILED...', error);
+        });
+    
+   
 }
 
-/* enviar email */
 
-var params= {
-    from: 'crisz_zanesky@hotmail.com',
-    to: 'criszanesky18@gmail.com',
-    message: 'Nuevo Usuario Creado Nombre: Pepe Apellido: Luis Email: luis@hotmail.com Contraseña: 1234',
-};
- 
-emailjs.send('service_9n5wu8g', 'template_zy2hlk8', params)
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       console.log('FAILED...', error);
-    });
+  
+        
+    
+
+
 
